8c109df4d343b178bb483bc9a68fab8dbf02f0a7
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-08-24 13:51:38 +0200

    added image on show card

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -7,10 +7,11 @@
             @guest
                 <h1>You must be logged first!</h1>
             @else
-            <div class="card">
-                <h5 class="card-header">{{ $project->topic }}</h5>
+            <div class="card w-50 mx-auto">
+                <img class="card-img-top" src="{{ $project->image }}" alt="{{ $project->title }}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $project->title }}</h5>
+                    <h6 class="card-title">{{ $project->topic }}</h6>
                     <p class="card-text text-secondary">{{ $project->date }}</p>
                     <p class="card-text">{{ $project->gitHub }}</p>
                     <a href="{{ route('projects.index') }}" class="btn btn-primary">Back to list</a>
